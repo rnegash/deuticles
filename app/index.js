c4f2085024b273e60da5127b398bad09e69f7921
rufael negash <rufael.negash@gmail.com>
2019-02-08 16:20:11 +0100

    working list with uniqe words

--- app/index.js
+++ app/index.js
@@ -4,15 +4,17 @@ const getWords = browser.storage.sync.get('list')
 function saveNewItem(item) {
 
   getWords.then((items, err) => {
-    const article = "das"
-    const noun = "boot"
-    const list = [
-      {
-        "article": article,
-        "noun": noun,
-      },
-      ...items.list
-    ]
+    const article = "der"
+    const noun = "tisch"
+    const { list: history } = items
+    console.log("items", history)
+    const list = {
+        [noun]: {
+          "article": article,
+          "noun": noun,
+        },
+        ...history
+      }
     browser.storage.sync.set({ list })
     getWords.then(items => console.log(items))
   })
