5e01380c8516d2ab781f0a674427a9e486665be5
rufael negash <rufael.negash@gmail.com>
2019-02-11 16:29:49 +0100

    remove need to check empy object

--- app/my-nouns.js
+++ app/my-nouns.js
@@ -1,16 +1,14 @@
 const getWords = browser.storage.sync.get('list')
 
 getWords.then(list => {
-
   const myWords = list.list
+
   Object.keys(myWords).forEach(key => {
     const { article, noun } = myWords[key]
-    const listElement = document.getElementById(checkEmpty(article))
+    const listElement = document.getElementById(article)
     const li = document.createElement('li')
     const articleWithNoun = document.createTextNode(`${article} ${noun}`)
     li.appendChild(articleWithNoun)
     listElement.appendChild(li)
   })
 })
-
-const checkEmpty = art => (art === '' ? 'das' : art )
