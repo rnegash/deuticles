a4ee26cfa1922f18129b3ff83108c13a4b389eb1
rufael negash <rufael.negash@gmail.com>
2019-02-11 16:07:29 +0100

    sort list by gender

--- app/my-nouns.js
+++ app/my-nouns.js
@@ -1,14 +1,16 @@
 const getWords = browser.storage.sync.get('list')
 
 getWords.then(list => {
-  const listElement = document.getElementById('list')
 
   const myWords = list.list
   Object.keys(myWords).forEach(key => {
-    const li = document.createElement('li')
     const { article, noun } = myWords[key]
+    const listElement = document.getElementById(checkEmpty(article))
+    const li = document.createElement('li')
     const articleWithNoun = document.createTextNode(`${article} ${noun}`)
     li.appendChild(articleWithNoun)
     listElement.appendChild(li)
   })
 })
+
+const checkEmpty = art => (art === '' ? 'das' : art )
